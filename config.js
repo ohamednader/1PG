dff535a7f50e4d3795d5d4ecca1e1dff2706478c
ohamednader <48139794+ohamednader@users.noreply.github.com>
2021-09-12 16:37:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-
+{
+  "bot": {
+    "id": "724041391445114932",
+    "secret": "UvOu3VrT23juWjAFuXnadi-MDJkJ4Ek0>",
+    "token": "NzI0MDQxMzkxNDQ1MTE0OTMy.Xu6aFw.EaYSsQctIQw3jYJX1v80d29LhWQ"
+  },
+  "dashboardURL": "https://protectionbot1s.herokuapp.com/,
+  "mongoURI": "mongodb+srv://user1:NDmkddfornEU9ATE@cluster0.n6hmf.mongodb.net/ProtectionBot-Heroku?retryWrites=true&w=majority"
+}
